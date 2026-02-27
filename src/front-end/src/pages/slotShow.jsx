8a4806b98315151815ecbe028ba176612602ed6d
abd3l3li <abdelaliell14@gmail.com>
2026-02-27 18:19:02 +0000

    some tailwindcss

--- src/front-end/src/pages/slotShow.jsx
+++ src/front-end/src/pages/slotShow.jsx
@@ -9,29 +9,33 @@ export default function SlotShow() {
     return (
         <div className="min-h-screen flex flex-col">
             <Navbar isLoggedIn={true} />
-            <main className="flex-1 flex flex-col items-start justify-center text-left gap-10 py-10">
+            <main className="flex-1 flex flex-col items-start 
+                                justify-center text-left gap-10 py-10 max-w-7xl ">
                 
                 <div className="flex items-center justify-start gap-10 w-full max-w-7xl px-5 text-left">
                     <Gallery orientation="horizontal" />    
-                    <div className="flex flex-col items-center gap-2 w-1/5">
-                        <img src={mapLogo} alt="map" className="mr-4 w-5 h-5 opacity-70" />
+                    <div className="space-y-2 flex flex-col items-start gap-3 ml-2 w-xl max-w-md s">
+                        <img src={mapLogo} alt="map" className="mr-4 w-7 h-7 opacity-80" />
                         <span className="text-lg font-semibold text-[var(--color-text)]">
                             {/* {stay.city} */}City
                         </span>
 
                         <div className="border-t border-[var(--color-muted)] opacity-50 w-full"></div>
 
-                        <span className="ml-4 text-lg font-bold text-[var(--color-text)]">
+                        <span className="ml-1 text-lg font-bold text-[var(--color-text)]">
                              {/* {stay.price ? `${stay.price} MAD` : "N/A"} */}Price
                         </span>
 
                         <div className="border-t border-[var(--color-muted)] opacity-50 w-full"></div>
 
-                        <div>
-                            <p className="text-sm text-[var(--color-muted)]">Available Slots</p>
-                            <span>
-                                {/* {stay.avSlots ? `${stay.avSlots} slots` : "N/A"} */}
+                        <div className="relative">
+                            <p className="mb-2 text-sm text-[var(--color-muted)]">Available Slots</p>
+                            <span className="relative flex items-center gap-2 text-lg font-bold text-[var(--color-text)]">
                                 <img src={slotsCircle} alt="slots" />
+                                <span className="absolute left-1/2 top-1/2 -ml-4 -translate-x-1/2 -translate-y-1/2 text-[var(--color-text)]">
+                                    {/* Your text here, e.g. slot count */}
+                                    5
+                                </span>
                             </span>
                         </div>
 
@@ -56,8 +60,12 @@ export default function SlotShow() {
                         </div>
 
                         <div className="border-t border-[var(--color-muted)] opacity-50 w-full"></div>
-        
+                        
+                        <button>a</button>
+                        <button>a</button>
+                        <button>a</button>        
                     </div>
+
 
                 </div>
             </main>
