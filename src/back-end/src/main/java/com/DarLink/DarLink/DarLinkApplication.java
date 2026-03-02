752c73ff44b8bd03ea6fc8fa081df65e7288eef9
Lc0d3r <younesscoder@gmail.com>
2026-03-02 23:12:47 +0000

    start 2026-03-02: start the real project

---
+++ src/back-end/src/main/java/com/DarLink/DarLink/DarLinkApplication.java
@@ -0,0 +1,13 @@
+package com.DarLink.DarLink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DarLinkApplication.class, args);
+	}
+
+}
