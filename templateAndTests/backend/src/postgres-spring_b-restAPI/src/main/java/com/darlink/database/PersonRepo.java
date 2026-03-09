78427a4b7a9a25ea7f12c20324dfe2c990a24079
Lc0d3r <younesscoder@gmail.com>
2026-03-09 07:34:14 +0000

    remove 2026-03-09: remove the template folder no need for it

--- templateAndTests/backend/src/postgres-spring_b-restAPI/src/main/java/com/darlink/database/PersonRepo.java
+++
@@ -1,8 +0,0 @@
-package com.darlink.database;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-// import com.darlink.database.Person;
-
-public interface PersonRepo extends JpaRepository<Person, Long> {
-
-}
