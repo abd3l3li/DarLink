f33c7a3bb7353091f6c51f18e3aa218c928b9ec3
Abdelouahedait <abdelouahedaitbo@gmail.com>
2026-03-15 13:47:51 +0000

    merge

--- src/front-end/src/components/utils/log_with_2.jsx
+++ src/front-end/src/components/utils/log_with_2.jsx
@@ -6,28 +6,6 @@ export default function Log_with() {
         window.location.href = "/";
     };
     return (
-<<<<<<< HEAD
-    <div className="flex gap-4 mt-8 ">
-        <button
-            type="button"
-            className="flex items-center justify-center
-                    rounded-full border bg-white
-                    transition-all duration-300 hover:shadow-md hover:-translate-y-0.5"
-                    onClick={handleClick} 
-            aria-label="Continue with Google">
-            <img src={Google} alt="Google" draggable="false"/>
-
-        </button>
-        <button 
-            type="button"
-            className="flex items-center justify-center
-                    rounded-full border bg-white
-                    transition-all duration-300 hover:shadow-md hover:-translate-y-0.5"
-            aria-label="Continue with Intra">
-            <img src={Intra} alt="Intra" draggable="false" />
-        </button>
-    </div>
-=======
         <div className="flex gap-4 mt-8 ">
             <a href="https://localhost:8443/oauth2/authorization/google">
                 <button
@@ -50,6 +28,5 @@ export default function Log_with() {
                 </button>
             </a>
         </div>
->>>>>>> 371ea7607147b17adb44e1ad69e774cb40039e85
     );
 }
